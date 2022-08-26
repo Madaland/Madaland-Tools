81e89d65965db7e633c476489177fe72b3846d48
Madaland <101234682+Madaland@users.noreply.github.com>
2022-08-26 09:24:23 +0800

    Create madaland.sol

---
+++ contracts/madaland/madaland.sol
@@ -0,0 +1,76 @@
+pragma solidity ^0.4.15;
+
+contract Token {
+    uint256 public totalSupply;
+
+    function balanceOf(address _owner) public constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract StandardToken is Token {
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    modifier onlyPayloadSize(uint numwords) {
+        assert(msg.data.length == numwords * 32 + 4);
+        _;
+    }
+
+    function transfer(address _to, uint256 _value)
+    public
+    returns (bool success) {
+        if (balances[msg.sender] >= _value && _value > 0 && balances[_to] + _value > balances[_to]) {
+            balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
+            balances[_to] = SafeMath.add(balances[_to], _value);
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)
+    public
+    returns (bool success) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0 && balances[_to] + _value > balances[_to]) {
+            balances[_to] = SafeMath.add(balances[_to], _value);
+            balances[_from] = SafeMath.sub(balances[_from], _value);
+            allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
+            Transfer(_from, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function balanceOf(address _owner)
+    public constant
+    returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value)
+    public
+    onlyPayloadSize(2)
+    returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+
+    function allowance(address _owner, address _spender)
+    public constant
+    onlyPayloadSize(2)
+    returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+}
