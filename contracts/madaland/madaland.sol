e0299e72a1b337603c99243d2a27b57f20630817
Madaland <101234682+Madaland@users.noreply.github.com>
2022-08-29 09:34:06 +0800

    Update madaland.sol

--- contracts/madaland/madaland.sol
+++ contracts/madaland/madaland.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.4.15;
 
 contract Token {
     uint256 public totalSupply;
-
     function balanceOf(address _owner) public constant returns (uint256 balance);
     function transfer(address _to, uint256 _value) public returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
