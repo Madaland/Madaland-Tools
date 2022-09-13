0735e98c72025cb97feae8689ad9a17daee12ddc
Madaland <101234682+Madaland@users.noreply.github.com>
2022-09-13 09:14:20 +0800

    Update madaland.sol

--- contracts/madaland/madaland.sol
+++ contracts/madaland/madaland.sol
@@ -7,7 +7,6 @@ contract Token {
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
     function approve(address _spender, uint256 _value) public returns (bool success);
     function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
-
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
